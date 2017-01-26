e5a321255983448be7461e240fee84be52f5f340
Reginaldo <reginaldo.junior696@gmail.com>
2017-01-26 11:57:36 -0200

    File changes

--- template/src/js/frontend/pages/Home.vue
+++ template/src/js/frontend/pages/Home.vue
@@ -15,7 +15,7 @@ export default {
 
 <template>
     <div id="app">
-        <p v-if="count > 0">Você clicou o botão {{ count }} {{ count > 1 ? 'vezes' : 'vez' }}</p>
+        <p v-if="count > 0">Você clicou o botão <span v-text="count"></span> <span v-text="count > 1 ? 'vezes' : 'vez'"></span></p>
         <br>
         <button @click.prevent="increment">add</button>
     </div>
